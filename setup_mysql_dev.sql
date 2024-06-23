042d67a5f85d21913f5af17f62c1ac125f30e7ea
sarah441 <sarahibrahim3477@gmail.com>
2024-06-23 17:23:30 +0300

    ff

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,5 +3,7 @@ CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 -- Create new user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Ensure all permissions on the entire database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'; FLUSH PRIVILEGES;
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'; FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'; 
+FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'; 
+FLUSH PRIVILEGES;
