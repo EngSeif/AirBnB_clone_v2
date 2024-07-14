c987a76faeb8bbbfd9c0d46fcb97b8b723f0ea4c
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-14 18:03:34 +0300

    AirBnb | Fix Func

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,55 +3,35 @@
 distributes an archive to your web servers,
 """
 
-from fabric.api import env, local, sudo, put, cd
+from fabric.api import env, local, sudo, put
 import os
 from datetime import datetime
-from fabric.contrib.files import exists
 
 env.hosts = ['18.207.1.248', '100.25.194.205']
 env.user = 'ubuntu'
 
 def do_deploy(archive_path):
     '''Deploy archive to web server'''
-    if not archive_path:
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        put(archive_path, '/tmp/')
+        archive_filename = archive_path.split('/')[-1]
+        archive_name_no_ext = archive_filename.split('.')[0]
+        release_path = '/data/web_static/releases/{}'.format(
+            archive_name_no_ext
+            )
+        sudo('mkdir -p {}'.format(release_path))
+        sudo('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
+        sudo('rm /tmp/{}'.format(archive_filename))
+        sudo('mv {}/web_static/* {}/'.format(release_path, release_path))
+        sudo('rm -rf {}/web_static'.format(release_path))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {} /data/web_static/current'.format(release_path))
+        print("New version deployed!")
+        return True
+    except Exception as e:
         return False
-
-    if not os.path.exists(archive_path):
-        print('Archive path does not exist:', archive_path)
-        return False
-
-    file_name = os.path.basename(archive_path)
-    file_name_no_ext = os.path.splitext(file_name)[0]
-    target_path = '/data/web_static/releases/{}'.format(file_name_no_ext)
-
-    # Upload the archive to the remote server
-    put(archive_path, '/tmp/', use_sudo=True)
-
-    # Create the target directory
-    if not exists(target_path):
-        sudo('mkdir -p {}'.format(target_path))
-
-    # Extract the archive into the target directory
-    with cd(target_path):
-        sudo('tar -xzf /tmp/{} -C {}'.format(file_name, target_path))
-
-    # Remove the uploaded archive from the remote server
-    sudo('rm /tmp/{}'.format(file_name))
-
-    # Move the contents of the extracted archive to the target directory
-    sudo('mv {}/web_static/* {}/'.format(target_path, target_path))
-
-    # Remove the web_static directory from the target directory
-    sudo('rm -rf {}/web_static'.format(target_path))
-
-    # Remove the current symlink if it exists
-    sudo('rm -rf /data/web_static/current')
-
-    # Create a new symlink to the latest release
-    sudo('ln -s {} /data/web_static/current'.format(target_path))
-
-    print('New version deployed!')
-    return True
 
 def do_pack():
     """
