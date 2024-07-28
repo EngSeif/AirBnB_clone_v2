c9aaf6420c53e6d178861b12bf639fb8a5059990
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-28 22:57:02 +0300

    web flask | fix 8V

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,22 @@
-#!/usr/bin/python3
-""" Simple Flask App"""
-from flask import Flask, render_template
-from models.state import State
+from flask import Flask, render_template_string
 from models import storage
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def Close_session(exception):
-    """
-    Close Session
-    """
+def teardown(exception):
+    """Closes the SQLAlchemy session after each request."""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def cities_by_states():
-    """
-    Display States
-    """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
+def states_list():
+    """Displays a HTML page with a list of all State objects."""
+    states = storage.all("State")
+    sorted_states = sorted(states.values(), key=lambda s: s.name)
+    return render_template_string('7-states_list.html', states=sorted_states)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
