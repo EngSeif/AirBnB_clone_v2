d226a870b9ed7f5988c0640212c0f1b16e70d2b0
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-28 18:47:26 +0300

    web flask | Return State Page | fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,10 +7,10 @@ from models import storage
 app = Flask(__name__)
 
 @app.teardown_appcontext
-def Close_session():
+def Close_session(exception):
     storage.close()
 
-@app.route('/cities_by_states', strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def cities_by_states():
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
