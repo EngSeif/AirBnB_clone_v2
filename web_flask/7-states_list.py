8b097de79701f959b6ec589d2ed69b44661c6bea
EngSeif <eng.seifmohamed2004@gmail.com>
2024-07-28 22:57:51 +0300

    web flask | fix 8V2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template_string
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ def teardown(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays a HTML page with a list of all State objects."""
-    states = storage.all("State")
+    states = storage.all(State)
     sorted_states = sorted(states.values(), key=lambda s: s.name)
     return render_template_string('7-states_list.html', states=sorted_states)
 
